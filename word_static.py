7bd5193c971f41ab533e3f637778639880a36a36
liushuchun <liuscgood@gmail.com>
2017-12-05 17:20:22 +0800

    update the result

--- word_static.py
+++ word_static.py
@@ -1,12 +1,15 @@
-import jieba
+import argparse
 from jieba import posseg as psg
 import operator
+import io
+import os
+
+include_extentions = ["jpeg.txt", "png.txt", "jpg.txt"]
 
 
 def load_stop_words(path="stop_words.utf8"):
-    from codecs import open
     stop_words = set()
-    with open(path, 'r', encoding='utf8') as f:
+    with io.open(path, 'r', encoding='utf8') as f:
         for l in f:
             l = l.strip()
             stop_words.add(l)
@@ -21,19 +24,17 @@ class WordAnalyzer:
         words = psg.cut(words)
         return [w for w, t in words if w not in self.stop_words]
 
-    def process(self, file_name, top_k=-1):
+    def process(self, file_name):
         """
-        
+
         :param file_name: file path
         :param top_k: if top_k=-1 print all, else top k
         :return: tuple
         """
         article = ""
-        with open(file_name, encoding='utf8') as f:
+        with io.open(file_name, encoding='utf8') as f:
             for line in f:
                 article += line.strip('\n')
-        print("here")
-        print(article)
 
         words = self.cut_words(article)
 
@@ -43,33 +44,79 @@ class WordAnalyzer:
             counts = word_statics.get(word, 0)
             word_statics[word] = counts + 1
 
-        sorted_words = sorted(word_statics.items(), key=operator.itemgetter(1), reverse=True)
+        sorted_words = sorted(word_statics.items(),
+                              key=operator.itemgetter(1), reverse=True)
 
-        assert (isinstance(top_k, int))
+        return sorted_words
 
-        if top_k < 0:
-            return sorted_words
-        else:
-            return sorted_words[0:top_k]
-
-    def show_plt(self, word_count):
+    def save_plt(self, word_count, image_path, top_k=-1):
+        import matplotlib
+        #matplotlib.use('Agg')
         import matplotlib.pyplot as plt
         from pylab import mpl
+
+        if top_k > 0:
+            word_count = word_count[:top_k]
         mpl.rcParams['font.sans-serif'] = ['FangSong']
         mpl.rcParams['axes.unicode_minus'] = False
 
+        fig = plt.figure()
         label = list(map(lambda x: x[0], word_count))
         value = list(map(lambda y: y[1], word_count))
         plt.bar(range(len(value)), value, tick_label=label)
-        plt.show()
+        # plt.show()
+        plt.savefig(image_path + '_word_statics.jpg')
+        plt.title('word static')
+        plt.close(fig)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Test OCR',
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--image', help='input image', default=None, type=str)
+    parser.add_argument(
+        '--image_folder', help='input image path', default=None, type=str)
+    parser.add_argument(
+        '--top_k', help='save top_k words in the word static image', default=10, type=int)
+    parser.add_argument(
+        '--result_folder', help='image result path', default="./result", type=str)
+    parser.add_argument(
+        '--save_to_txt', help='save the tuple to the txt file', default=True, type=bool)
+
+    args = parser.parse_args()
+    return args
 
 
 if __name__ == "__main__":
+    args = parse_args()
+    args.save_to_txt=True
+
     analyzer = WordAnalyzer()
 
-    words_tuple = analyzer.process("ocr_v4_recog/weibo/005Zu0d2ly1fliuvdf9k8j30hs2p515r.jpg.txt")
+    results = {}
 
-    print(words_tuple)
-    analyzer.show_plt(words_tuple[:10])
+    if args.image is not None:
+        image_path = args.image
+        text_path = image_path + '.txt'
+        words_tuple = analyzer.process(text_path)
+        analyzer.save_plt(words_tuple, image_path, args.top_k)
+    else:
 
+        file_list = [fn for fn in os.listdir(args.image_folder) if any(fn.endswith(ext) for ext in include_extentions)]
 
+        for f in file_list:
+
+            words_tuple = analyzer.process(os.path.join(args.image_folder, f))
+
+            analyzer.save_plt(words_tuple, os.path.join(args.result_folder,f), args.top_k)
+
+            if args.save_to_txt:
+                results[f] = words_tuple
+
+        if args.save_to_txt:
+            import json
+
+            js = json.dumps(results)
+
+            with open("output.txt", "w+") as file:
+                file.write(str(js))
